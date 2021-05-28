8bf395c18233ebf61da961d0bfc2a2e56e8e9a9f
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2021-05-28 14:30:16 -0300

    Remove not used functions

--- stakingpool/stakingpool.sol
+++ stakingpool/stakingpool.sol
@@ -118,23 +118,6 @@ contract StakingPool is IStakingRewards, ReentrancyGuard, Pausable {
         notifyRewardAmount(rewardForNextDuration);
     }
 
-    // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
-    function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
-        require(tokenAddress != address(stakingToken), "Cannot withdraw the staking token");
-        IERC20(tokenAddress).safeTransfer(owner, tokenAmount);
-        emit Recovered(tokenAddress, tokenAmount);
-    }
-
-    // Set the time interval for rewards to be distributed
-    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
-        require(
-            block.timestamp > periodFinish,
-            "Previous rewards period must be complete before changing the duration for the new period"
-        );
-        rewardsDuration = _rewardsDuration;
-        emit RewardsDurationUpdated(rewardsDuration);
-    }
-
     /* ========== MODIFIERS ========== */
 
     // Update the reward for a given Account
